8018451de1d0cc3b737b32c569f500aeef76a61e
miguelmif <miguelfracassi@gmail.com>
2022-06-29 08:36:32 -0300

    pag 10 jahtml

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function mensagem()
+    {
+        return $this->hasMany(Mensagem::class);
+    }
 }
---
+++ resources/views/restrict/layout.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reuse</title>
+    <link rel="stylesheet" href="{{ asset('css/restrict/estilo.css') }}">
+</head>
+
+<body>
+    <header>
+        <picture>
+            <img src="{{asset('img;logo.svg')}}" alt="Logo" />
+        </picture>
+        <nav>
+            <ul>
+                <li>
+                    <a href="{{url('/mensagem)}}">Mensagens</a>
+                </li>
+                <li>
+                    <a href="{{url('/dashboard)}}">Usuários</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+         @yield('content')
+    </main>
+</body>
+
+</html>
+            
