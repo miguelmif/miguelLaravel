62897b8ec06d6e124f40783010fe66b717357b6e
Miguel Miranda Fracassi <miguelfracassi@gmail.com>
2022-06-30 22:56:31 -0300

    terminei pag 11

---
+++ resources/views/restrict/mensagem/create.blade.php
@@ -0,0 +1,36 @@
+@extends('restrict.layout')
+
+@section('content')
+@if(count($errors) > 0)
+<ul class="validator">
+    @foreach($errors->all() as $error)
+    <li>{{$error}}</li>
+    @endforeach
+</ul>
+@endif
+<form method="POST" action="{{url('mensagem')}}" enctype="multipart/form-data">
+    @crsf
+    @method('POST')
+    <div>
+        <label for="titulo">Título</label>
+        <input type="text" name="titulo" id="titulo" valeu="{{old('titulo')}}" required />
+    </div>
+    <div>
+        <label for="msg">Mensagem</label>
+        <textarea name="mensagem" id="msg" required>{{old('mensagem')}}</textarea>
+    </div>
+    <div>
+        <label>
+            Tópicos
+            <a href="{{rul('topico/create')}}" class="button">Add Tópico</a>
+        </label>
+        <div class="sub">
+            @foreach($topicos as $topico)
+            <input type="checkbox" id="top{{$topico->id}}" value="{{$topico->id}}" name="topico[]" @if($topico->id==old('topico_id')) checked @endif />
+            <label for="top{{$topico->id}}">{{$topico->topico}}</label>
+            @endforeach
+        </div>
+    </div>
+    <button type="submit" class="button">Salvar</button>
+</form>
+@endsection
