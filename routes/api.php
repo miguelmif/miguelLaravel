64e3c1c6e26d85e548bf12e62b7bcaa80eca88d4
Miguel Miranda Fracassi <miguelfracassi@gmail.com>
2022-08-29 18:07:08 -0300

    finalmente

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\MensagemController;
+use App\Http\Controllers\API\TopicoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,10 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/logout', [AuthController::class, 'logout']);
+    Route::resource("topico",TopicoController::class);
+    Route::resource("mensagem",MensagemController::class);
+
 });
-
-
-
