5af9c95e0d63ee24b51c970c0de12fcf09207a96
miguelmif <miguelfracassi@gmail.com>
2022-09-14 10:42:25 -0300

    drive

--- app/Http/Controllers/MensagemController.php
+++ app/Http/Controllers/MensagemController.php
@@ -1,12 +1,12 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Models\Mensagem;
 use App\Models\Topico;
 use Illuminate\Http\Request;
-use Illuminate\Support\facades\Auth;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+
 
 class MensagemController extends Controller
 {
@@ -51,12 +51,12 @@ class MensagemController extends Controller
             $mensagem->user_id = Auth::user()->id;
             $mensagem->titulo = $request->get('titulo');
             $mensagem->mensagem = $request->get('mensagem');
-            //$name = $request->file('imagem')->getClientOriginalName();
-            //$path = $request->file('imagem')->storeAs("public/img", $name);
-            $name = $request->file('imagem')->store('', 's3');
-            Storage::disk('s3')->setVisibility($name, 'public');
-            $path = Storage::disk('s3')->url($name);
-            $mensagem->imagem = $path;     
+            // $name = $request->file('imagem')->getClientOriginalName();
+            // $path = $request->file('imagem')->storeAs("public/img", $name);
+            $name = $request->file('imagem')->store('', 'google');
+            Storage::disk('google')->setVisibility($name, 'public');
+            $path = Storage::disk('google')->url($name);
+            $mensagem->imagem = $path; 
             $mensagem->save();
             $mensagem->topicos()->attach($request->get('topico'));
             return redirect('mensagem');
@@ -104,12 +104,12 @@ class MensagemController extends Controller
         if ($validated) {
             $mensagem->titulo = $request->get('titulo');
             $mensagem->mensagem = $request->get('mensagem');
-            //$name = $request->file('imagem')->getClientOriginalName();
-            //$path = $request->file('imagem')->storeAs("public/img", $name);
-            $name = $request->file('imagem')->store('', 's3');
-            Storage::disk('s3')->setVisibility($name, 'public');
-            $path = Storage::disk('s3')->url($name);
-            $mensagem->imagem = $path;
+            // $name = $request->file('imagem')->getClientOriginalName();
+            // $path = $request->file('imagem')->storeAs("public/img", $name);
+            $name = $request->file('imagem')->store('', 'google');
+            Storage::disk('google')->setVisibility($name, 'public');
+            $path = Storage::disk('google')->url($name);
+            $mensagem->imagem = $path; 
             $mensagem->save();
             $mensagem->topicos()->sync($request->get('topico'));
             return redirect('mensagem');
--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,14 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
+        'google' => [
+            'driver' => 'goggle',
+            'clientId' => env('GOOGLE_DRIVE_CLIENT_SECRET'),
+            'clientScret' => env('GOOGLE_DRIVE_CLIENT_SECRET'),
+            'refreshToken' => env('GOOGLE_DRIVE_REFRESH_TOKEN'),
+            'folder' => env('GOOGLE_DRIVE_FOLDER'),
+            'accessToken' => env('GOOGLE_DRIVE_ACCESS_TOKEN')
+        ],
 
     ],
 
